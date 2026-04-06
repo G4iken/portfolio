488252be54a77b52e8a2ad3f226af82a192ad532
G4iken <ebardojeremyelmo@gmail.com>
2026-04-06 20:00:50 +0800

    Fix body background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import useTheme  from './hooks/useTheme'
+import { useTheme } from './hooks/useTheme'
 import Navbar from './components/sections/Navbar'
 import Hero from './components/sections/Hero'
 import About from './components/sections/About'
@@ -6,7 +6,7 @@ import Projects from './components/sections/Projects'
 import Education from './components/sections/Education'
 import Contact from './components/sections/Contact'
 import Footer from './components/sections/Footer'
-import ChatBot from './ChatBot'
+import ChatBot from './components/sections/ChatBot'
 
 export default function App() {
   const { isDark, toggle } = useTheme()
